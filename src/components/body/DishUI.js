d853d4e758e9f730f14d42dc03769bce0acf9f06
Hyun Jong Kim <Aya@Hyuns-MacBook-Pro.local>
2017-07-06 13:47:53 +1200

    implemented text input. They are great

--- src/components/body/DishUI.js
+++ src/components/body/DishUI.js
@@ -59,38 +59,39 @@ class DishUI extends React.Component {
                 const dishName = this.state.tempName;
                 const dishId = props.id;
                 const dataArr = [dishId, dishName];
-                console.log("we"+ props.id);
+                console.log("we" + props.id);
                 FoodActions.editDish(dataArr);
                 this.setState({
                     isEditing: false
-                });                
+                });
             }
         }
     }
-// Refs: https://facebook.github.io/react/docs/refs-and-the-dom.html
+    // Refs: https://facebook.github.io/react/docs/refs-and-the-dom.html
     dishDisplay(props) {
         if (this.state.isEditing) {
             const tempName = this.state.tempName;
-                return <ContentEditable
-                    html={tempName}
-                    disabled={false}
-                    onChange={(e)=>this.editHandler(e)}
-                    onKeyPress={(evt) => this.handleEditEnter(props, evt)}
-                    autoFocus
-                />
-            
+            return <input
+                onChange={(e) => this.editHandler(e)}
+                onKeyPress={(evt) => this.handleEditEnter(props, evt)}
+                value={tempName}
+                autoFocus
+            ></input>
+
         } else {
             const name = props.name;
             return <div><span> {name}  </span> <button onClick={(event) => this.editStateHandler(props, event)}> Edit </button></div>
-        }
+                }
     }
 
     editStateHandler = (props, event) => {
         if (this.state.isEditing == false) {
-            this.setState({ isEditing: true, 
-            tempName: props.name }
-            );
-        }
+                    this.setState({
+                        isEditing: true,
+                        tempName: props.name
+                    }
+                    );
+                }
     }
 
     render() {
@@ -98,44 +99,44 @@ class DishUI extends React.Component {
         const id = dish.id;
         return (
             <div className="row">
-                <div className="col-sm-8 dish" onClick={this.handleClick}>
-                    {this.dishDisplay(dish)}
+                    <div className="col-sm-8 dish" onClick={this.handleClick}>
+                        {this.dishDisplay(dish)}
+                    </div>
+                    <button onClick={() => this.deleteDish(id)}> delete! </button>
+                    {this.expand(dish)}
                 </div>
-                <button onClick={() => this.deleteDish(id)}> delete! </button>
-                {this.expand(dish)}
-            </div>
-        );
+                );
     }
 }
-// Passing an extra argument with event handler: 
+// Passing an extra argument with event handler:
 // https://stackoverflow.com/questions/28331102/pass-additional-arguments-to-event-handler
 
 
 class AddDishUI extends React.Component {
-    render() {
-        const contentEdit = this.props.contentEdit;
+                    render() {
+                const contentEdit = this.props.contentEdit;
         const newDish = this.props.newDish;
         const addDish = this.props.addDish;
         const click = this.props.click;
         const keyPress = this.props.keyPress;
         return (
             <div className="row">
-                <div className="col-sm-8 dish">
-                    <ContentEditable
-                        html={newDish}
-                        disabled={false}
-                        onChange={contentEdit}
-                        onClick={click}
-                        onKeyPress={keyPress}
-                    />
+                    <div className="col-sm-8 dish">
+                        <ContentEditable
+                            html={newDish}
+                            disabled={false}
+                            onChange={contentEdit}
+                            onClick={click}
+                            onKeyPress={keyPress}
+                        />
+                    </div>
+                    <button onClick={addDish}>add!</button>
                 </div>
-                <button onClick={addDish}>add!</button>
-            </div>
-        );
+                );
     }
 }
 
 module.exports = {
-    dishUI: DishUI,
+                    dishUI: DishUI,
     addDish: AddDishUI
 }
